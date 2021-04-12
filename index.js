d6c7642a7e4baaa9bc7cb7ffa82913edf768b6c8
Nena <nena0undefined@gmail.com>
2021-04-13 01:23:26 +0900

    Update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
+console.log("hoge")
+
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
