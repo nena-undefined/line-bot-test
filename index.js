242a42cc2c799e8892eb3c9d680330a2a11c6049
Nena <nena0undefined@gmail.com>
2021-04-13 01:28:18 +0900

    Update

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-console.log("hoge")
+console.log("hoge");
 
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
-
+console.log("fuga");
 // create LINE SDK client
 const client = new line.Client(config);
-
+console.log("monge");
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
