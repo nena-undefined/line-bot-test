bdada75ad14acd5a6f302bee6a64597953a715c3
Nena <nena0undefined@gmail.com>
2021-04-13 01:33:26 +0900

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log("hoge");
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 console.log("fuga");
