b0c6fc0e86000ecc9ea1e67fd1bbc17d25574898
Nena <nena0undefined@gmail.com>
2021-04-13 00:03:48 +0900

    Update

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-// -----------------------------------------------------------------------------
-// モジュールのインポート
-const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+  
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
-const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const app = express();
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
 };
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-server.listen(process.env.PORT || 3000);
-
-//弄る
 const client = new line.Client(config);
-
-async function handleEvent(event) {
+function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
 
+  const suffix = [
+    "ってなんやねん！",
+    "か。わかるわ。",
+    "とか怖いなぁ。",
+    "とかやめてくれや。",
+    "ってめっちゃいいやん…。",
+    "は最高じゃんか。",
+  ];
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
+    text: event.message.text + suffix[Math.floor(Math.random() * suffix.length)]
   });
 }
 
+app.post('/', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+})
 
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log(req.body);
-});
+module.exports = app;
