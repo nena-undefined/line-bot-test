e7d588e7d14c78d2d313fe89520a0f556c24b83b
Nena <nena0undefined@gmail.com>
2021-04-12 23:56:45 +0900

    Update

--- index.js
+++ index.js
@@ -14,10 +14,33 @@ const line_config = {
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
+//弄る
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text //実際に返信の言葉を入れる箇所
+  });
+}
+
 
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
     console.log(req.body);
+    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
+      res.send('Hello LINE BOT!(POST)');
+      console.log('疎通確認用');
+      return; 
+  }
+
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
 });
