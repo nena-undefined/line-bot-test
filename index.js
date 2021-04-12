9c785389f9f2943ed97296452fd01725f4214ea4
Nena <nena0undefined@gmail.com>
2021-04-12 22:52:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ server.listen(process.env.PORT || 3000);
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
-    console.log(line_config)
+    console.log(req.body)
 });
